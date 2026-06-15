05d1e2eeac3c5114664d26f7344b9813d79aa140
elianmsalas <emunozsalas@icloud.com>
2026-06-14 20:53:12 -0400

    fix: agregar ruta raíz con Navigate e importar Navigate desde react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Login      from './pages/Login'
 import Register   from './pages/Register'
 import Posts      from './pages/Posts'
 import CrearPost  from './pages/CreatePost'
+import PostDetalle from './pages/PostDetalle'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/"            element={<Navigate to="/posts" />} />
         <Route path="/login"       element={<Login />} />
         <Route path="/register"    element={<Register />} />
         <Route path="/posts"       element={<Posts />} />
         <Route path="/posts/nuevo" element={<CrearPost />} />
+        <Route path="/posts/:slug"   element={<PostDetalle />} />
       </Routes>
     </BrowserRouter>
   )
